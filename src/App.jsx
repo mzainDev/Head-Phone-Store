2118946e5f321e4c68a8c77f5529f1e9a02c089e
Muhammad Zain <rubencallen689@gmail.com>
2024-12-26 19:29:28 +0500

    build by Headphone store

---
+++ src/App.jsx
@@ -0,0 +1,28 @@
+import './App.css'
+import Hero from './components/Hero/Hero'
+import Navbar from './components/Navbar/Navbar'
+import { UpdateFollower  } from 'react-mouse-follower'
+
+function App() {
+
+  return (
+    <>
+    <main className='overflow-x-hidden'>
+      <UpdateFollower
+      mouseOptions = {{
+        backgroundColor: "white",
+        zIndex: 999,
+        followSpeed: 1.5,
+      }}
+      >
+      <Navbar/>
+      <Hero/>
+      </UpdateFollower>
+    </main>
+
+
+    </>
+  )
+}
+
+export default App
---
+++ src/components/Hero/Hero.jsx
@@ -0,0 +1,230 @@
+import React from "react";
+import Headphone1 from "../../assets/headphone.png";
+import Headphone2 from "../../assets/headphone2.png";
+import Headphone3 from "../../assets/headphone3.png";
+import { FaWhatsapp } from "react-icons/fa";
+import { UpdateFollower } from "react-mouse-follower";
+import { AnimatePresence, delay, easeInOut, easeOut, motion } from "framer-motion";
+
+const fadeUp = (delay = 0) => {
+  return {
+    hidden: {
+      opacity: 0,
+      y: 100,
+      scale: 0.5,
+    },
+    show: {
+      opacity: 1,
+      y: 0,
+      scale: 1,
+      transition: {
+        duration: 0.5,
+        delay: delay,
+        ease: easeInOut,
+      },
+    },
+  };
+};
+
+const headphoneData = [
+  {
+    id: 1,
+    image: Headphone1,
+    title: "Headphone Wireless",
+    subtitle:
+      "Lorem ipsum dolor sit, amet consectetur adipisicing elit. Nobis odit perspiciatis sapiente eveniet delectus officiis architecto, beatae a fugiat similique vero neque facilis. Repellat, nemo laboriosam pariatur eveniet ex voluptas.",
+    price: "$100",
+    modal: "Modal Brown",
+    bgColor: "#8b5958",
+  },
+  {
+    id: 2,
+    image: Headphone2,
+    title: "Headphone Wireless 2",
+    subtitle:
+      "Lorem ipsum dolor sit, amet consectetur adipisicing elit. Nobis odit perspiciatis sapiente eveniet delectus officiis architecto, beatae a fugiat similique vero neque facilis. Repellat, nemo laboriosam pariatur eveniet ex voluptas.",
+    price: "$100",
+    modal: "Lime Green",
+    bgColor: "#638153",
+  },
+  {
+    id: 3,
+    image: Headphone3,
+    title: "Headphone Wireless 3",
+    subtitle:
+      "Lorem ipsum dolor sit, amet consectetur adipisicing elit. Nobis odit perspiciatis sapiente eveniet delectus officiis architecto, beatae a fugiat similique vero neque facilis. Repellat, nemo laboriosam pariatur eveniet ex voluptas.",
+    price: "$100",
+    modal: "Ocean Blue",
+    bgColor: "#5d818c",
+  },
+];
+
+const Hero = () => {
+  const [activeData, setActiveData] = React.useState(headphoneData[0]);
+
+  const handleActiveData = (data) => {
+    setActiveData(data);
+  };
+
+  return (
+    <>
+      <section className="bg-brandDark text-white font-varela">
+        <div className="container grid grid-cols-1 md:grid-cols-2 min-h-[700px]">
+          {/* headphoneInfo */}
+
+          <div className="flex flex-col justify-center py-14 md:py-0 xl:max-w-[500px]">
+            <div className="space-y-5 text-center md:text-left">
+              <AnimatePresence mode="wait">
+                <UpdateFollower
+                  mouseOptions={{
+                    backgroundColor: "white",
+                    zIndex: 9999,
+                    followSpeed: 0.5,
+                    rotate: -720,
+                    mixBlendMode: "difference",
+                    scale: 10,
+                  }}
+                >
+                  <motion.h1
+                    key={activeData.id}
+                    variants={fadeUp(0.2)}
+                    initial="hidden"
+                    animate="show"
+                    exit="exit"
+                    className="text-3xl lg:text-6xl font-bold font-varela"
+                  >
+                    {activeData.title}
+                  </motion.h1>
+                </UpdateFollower>
+              </AnimatePresence>
+
+              <AnimatePresence mode="wait">
+                <motion.p
+                  key={activeData.id}
+                  variants={fadeUp(0.2)}
+                  initial="hidden"
+                  animate="show"
+                  exit="exit"
+                  className="text-sm leading-loose text-white/80"
+                >
+                  {activeData.subtitle}
+                </motion.p>
+              </AnimatePresence>
+
+              {/* Headphone Title */}
+
+              <AnimatePresence mode="wait">
+                <UpdateFollower
+                  mouseOptions={{
+                    backgroundColor: activeData.bgColor,
+                    zIndex: 9999,
+                    followSpeed: 0.5,
+                    rotate: -720,
+                    mixBlendMode: "difference",
+                    scale: 6,
+                    backgroundElement: (
+                      <div>
+                        <img src={activeData.image} />
+                      </div>
+                    ),
+                  }}
+                >
+                  <motion.button
+                    key={activeData.id}
+                    variants={fadeUp(0.3)}
+                    initial="hidden"
+                    animate="show"
+                    exit="exit"
+                    style={{
+                      backgroundColor: activeData.bgColor,
+                    }}
+                    className="px-4 py-2 inline-block font-normal rounded-sm"
+                  >
+                    Buy and Listen
+                  </motion.button>
+                </UpdateFollower>
+              </AnimatePresence>
+
+              {/* Headphone List Seperator */}
+
+              <div className="flex items-center justify-center md:justify-start gap-4 !mt-24">
+                <div className="w-20 h-[1px] bg-white"></div>
+                <p className="uppercase text-sm">Top headphone for you</p>
+                <div className="w-20 h-[1px] bg-white"></div>
+              </div>
+
+              {/* Headphone list switcher */}
+
+              <div className="grid grid-cols-3 gap-10">
+                {headphoneData.map((item) => {
+                  return (
+                    <UpdateFollower
+                      mouseOptions={{
+                        backgroundColor: item.bgColor,
+                        zIndex: 9999,
+                        followSpeed: 0.5,
+                        scale: 5,
+                        text: "view Detail",
+                        textFontSize: "3px",
+                      }}
+                    >
+                      <div
+                        key={item.id}
+                        onClick={() => handleActiveData(item)}
+                        className="grid grid-cols-2 place-items-center cursor-pointer"
+                      >
+                        <div>
+                          <img src={item.image} alt="" />
+                        </div>
+                        <div className="space y-2">
+                          <p className="text-base font-bold ">{item.price}</p>
+                          <p className="text-xs font-normal text-nowrap">
+                            {item.modal}
+                          </p>
+                        </div>
+                      </div>
+                    </UpdateFollower>
+
+                  );
+                })}
+              </div>
+            </div>
+          </div>
+
+          {/* HeroImage */}
+          <div className="flex flex-col justify-end items-center">
+            <AnimatePresence mode="wait">
+              <motion.img
+                key={activeData.id}
+                initial={{ opacity: 0, scale: 0.9, y: 100 }}
+                animate={{ opacity: 1, scale: 1, y: 0 }}
+                transition={{ duration: 0.4, delay: 0.2, ease: easeOut }}
+                exit={{
+                  opacity: 0,
+                  scale: 0.9,
+                  y: 100,
+
+                  transition: {
+                    duration: 0.2,
+                  },
+
+                }}
+                src={activeData.image}
+                alt=""
+                className="w-[300px] md:w-[400px] xl:w-[550px]"
+              />
+            </AnimatePresence>
+          </div>
+          {/* WhatsappIcon */}
+          <div className="text-3xl text-white fixed bottom-10 right-10 hover:rotate-[360deg] duration-500 z-[99999] mix-blend-difference">
+            <a href="">
+              <FaWhatsapp />
+            </a>
+          </div>
+        </div>
+      </section>
+    </>
+  );
+};
+
+export default Hero;
